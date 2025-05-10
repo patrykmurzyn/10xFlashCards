85f7e0a0199053a60539743e798d85eebcc3b2d7
Patryk Murzyn <patrykmurzyn@pm.me>
2025-05-10 22:30:54 +0200

    chore: Update Playwright configuration to adjust server command and URL based on CI environment

--- playwright.config.ts
+++ playwright.config.ts
@@ -83,7 +83,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run dev",
+    command: process.env.CI ? "npm run dev -- --port 3000" : "npm run dev",
     url: process.env.CI ? "http://localhost:3000" : "http://localhost:4321",
     reuseExistingServer: !process.env.CI,
     timeout: 120000,
